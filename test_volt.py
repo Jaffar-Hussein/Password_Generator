79981f106317ab7f8be6983f973760bba20defa1
Jaffar Hussein <jaffhussein@icloud.com>
2022-04-25 15:05:17 +0200

    ft:test_cases - test cases for the credentials class

--- test_volt.py
+++ test_volt.py
@@ -2,7 +2,6 @@ import unittest
 from volt import *
 
 
-
 class VoltTest(unittest.TestCase):
     """
     Tests for the volt module with the the two classes User and Credentials
@@ -14,10 +13,35 @@ class VoltTest(unittest.TestCase):
         Runs before the test cases to initialize the different parameters to
         be tested
         """
+        self.new_credential = Credentials('instagram', '123456', 'hanush')
+        self.value_returned = Credentials.delete_credential('instagram')
+        self.length_dictionary = len(Credentials.password_vault)
+        self.display_dict = Credentials.display_credentials()
         self.new_user = User('hanan', 'jaffar')
         self.authenticate_false = User.authenticate('hanan', 'hussein')
         self.authenticate_true = User.authenticate('hanan', 'jaffar')
         self.auto_generated_password = Credentials.auto_generate_password(9)
+
+    def test_credentials(self):
+        """
+        Checks if the credentials class is creating instances of new credentials
+        """
+        self.assertTrue(isinstance(self.new_credential, Credentials))
+
+    def test_deletion_of_credentials(self):
+        """
+        Checks if the credentials can be deleted as the function returns the account name
+        and not False
+        and also checking if the dictionary holding accounts is empty 
+        """
+        self.assertTrue(self.value_returned)
+        self.assertEqual(self.length_dictionary, 0)
+
+    def test_display(self):
+        """
+        Checks if the display_credentials class returns a dictionary for the list of credentials
+        """
+        self.assertisNotNone(self.display_dict)
 
     def test_User(self):
         """
